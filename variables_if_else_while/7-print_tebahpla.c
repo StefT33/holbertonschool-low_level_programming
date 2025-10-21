f7663279ef7ed58cbb4a4a86193ab3a1754fad07
StefT33 <12093@holbertonstudents.com>
2025-10-21 19:18:36 +0000

    ajout de 7-

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - s'affiche en sortie
+* Return: 0
+*/
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch <= 'a'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
