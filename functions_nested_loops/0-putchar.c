c4d9cee9a4b436ae26946e828c467aae981c5825
StefT33 <12093@holbertonstudents.com>
2025-10-22 12:29:29 +0000

    ajout de 0-

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main - entry point
+* Descrption: print _putchar, followed by a new line
+*
+* Return: 0;
+*/
+int main(void)
+{
+const char *text = "_putchar\n";
+write(1, text, 9)
+return (0);
+}
