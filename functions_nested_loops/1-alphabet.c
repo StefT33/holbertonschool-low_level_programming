ba94e270645152f3c43e9133f099ea875be1afef
StefT33 <12093@holbertonstudents.com>
2025-10-22 13:47:17 +0000

    ajout de 1-

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 #include "main.h"
 /**
-* main - entry point
-* Descrption: print alphabet, followed by a new line
+* void print_alphabet: print alphabet, followed by a new line
 *
 * Return: 0;
 */
-int main(void)
+void print_alphabet(void)
 {
-const char *text = "abcdefghijklmnopqrstuvwxyz\n";
-write(1, text, 27);
-return (0);
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
