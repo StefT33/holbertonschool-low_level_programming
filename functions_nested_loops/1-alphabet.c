b520fcc4ff76aee80213c4735dcb31d1218549e1
StefT33 <12093@holbertonstudents.com>
2025-10-22 12:47:11 +0000

    ajout de 1-

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main - entry point
+* Descrption: print alphabet, followed by a new line
+*
+* Return: 0;
+*/
+int main(void)
+{
+const char *text = "abcdefghijklmnopqrstuvwxyz\n";
+write(1, text, 27);
+return (0);
+}
