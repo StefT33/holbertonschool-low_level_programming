7894cccc0750eee284c5251c517f80b3a031cba0
StefT33 <12093@holbertonstudents.com>
2025-10-23 20:04:40 +0000

    ajout de 4-

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include "main.h"
+/**
+* _isalpha - checks if character is a letter
+* @c: character to be checked
+* Return: 1 ou 0
+*/
+
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
