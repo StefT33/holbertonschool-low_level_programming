5fc2cda82feb35bbd7a5c34db2f8610196093bbc
StefT33 <12093@holbertonstudents.com>
2025-10-24 21:45:20 +0000

    ajout de 10-

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -5,12 +5,11 @@
 * @a: int to be added
 * @b: int to be added
 * somme: result of a+b
-* Return: 0
+* Return: somme
 */
 int add(int a, int b)
 {
 int somme;
 somme = a + b;
-printf(" %d", somme);
 return (somme);
 }
