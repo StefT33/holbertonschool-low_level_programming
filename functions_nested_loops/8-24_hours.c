4e9640e37c1c1eec332ed1ca9270c89969dcc5af
StefT33 <12093@holbertonstudents.com>
2025-10-24 10:29:16 +0000

    ajout de 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,11 +2,11 @@
 #include "main.h"
 /**
 * jack_bauer - prints minutes of the day, starting from 00:00 to 23:59.
-* @um: unite minutes to be printed
-* @dm: dizaine minutes to be printed
-* @uh: unite hours to be printed
-* @dh: dizaine hours to be printed
-* Return: voi
+* um: unite minutes to be printed
+* dm: dizaine minutes to be printed
+* uh: unite hours to be printed
+* dh: dizaine hours to be printed
+* Return: void
 */
 void jack_bauer(void)
 {
@@ -15,8 +15,8 @@ for (h = 0 ; h <= 23 ; h++)
 {
 for (m = 0 ; m <= 59 ; m++)
 {
-int dh = h /10, uh = h % 10;
-int dm = m /10, um = m % 10;
+int dh = h / 10, uh = h % 10;
+int dm = m / 10, um = m % 10;
 _putchar (dh + '0');
 _putchar (uh + '0');
 _putchar (':');
