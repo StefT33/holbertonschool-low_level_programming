8cec94275450251425092ea37382ae43c6b19bc7
StefT33 <12093@holbertonstudents.com>
2025-10-29 13:53:45 +0000

    ajout de 6- et main

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, starting with the first character
+ *
+ * Return: Always 0.
+ */
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);
-void rev_string(char *s);
+void puts2(char *str);
 #endif
