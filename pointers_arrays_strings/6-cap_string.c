476868d47e8b0db247bf3f24c43f3c34614d1302
StefT33 <12093@holbertonstudents.com>
2025-10-31 22:28:19 +0000

    ajout de 6-

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,13 +3,35 @@
 
 /**
  * *cap_string - capitalizes all words of a string
- *
+ * @s: string
  * Return: void
  */
 char *cap_string(char *s)
 {
-  int i = 0;
+int i = 0;
+int j;
+int deb = 0;
+char sep[] = " \t\n,;.!?\"(){}";
 
-  while (s[i] != '\0')
-    i++;
-  if (
+while (s[i] != '\0')
+{
+if (i == 0)
+deb = 1;
+else
+{
+for (j = 0; sep[j] != '\0'; j++)
+{
+if (s[i - 1] == sep[j])
+{
+deb = 1;
+break;
+}
+}
+}
+
+if (deb && s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+i++;
+}
+return (s);
+}
