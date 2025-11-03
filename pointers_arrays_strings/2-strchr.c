3c4983a111f031fbfef70e7ccf2ff1940b18ad82
StefT33 <12093@holbertonstudents.com>
2025-11-03 11:09:18 +0000

    ajout de files

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to locate
+ * Return: pointer or NULL
+ */
+char *_strchr(char *s, char c)
+{
+char *p = s;/**pointeur courant qui pointe vers debut de s*/
+while (*p != '\0')
+{
+if (*p == c)
+return (p);
+else
+p++;
+}
+return (NULL);
+}
