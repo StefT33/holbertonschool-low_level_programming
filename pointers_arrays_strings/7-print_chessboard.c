d0c505f39a8673952fcda0304ca63e5303b32fd4
StefT33 <12093@holbertonstudents.com>
2025-11-04 14:17:33 +0000

    ajout de files

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_chessboard - prints the chessboard.
- * @*a: 
+ * @a: board
  */
 void print_chessboard(char (*a)[8])
 {
-  char (*row)[8] = a;
-  char
-  for (; row < 8; row++)
+char (*row)[8] = a;
+char *col;
+for (; row < a + 8; row++)
+{
+col = *row;
+for (; col < *row + 8; col++)
+_putchar(*col);
+_putchar('\n');
+}
+}
