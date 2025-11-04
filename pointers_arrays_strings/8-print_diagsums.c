1a9a8a4bcbfd6e9d9d1bfd8c92c3627b985e34d9
StefT33 <12093@holbertonstudents.com>
2025-11-04 15:16:00 +0000

    ajout de 8-

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: 2D array
+ * @size: size of square
+ */
+void print_diagsums(int *a, int size)
+{
+  int i;
+  for (i = 0; i < size; i++)
+    
