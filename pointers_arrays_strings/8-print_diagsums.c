a378f7ac1fb47ae730eea9e6756dab80e5b56f7a
StefT33 <12093@holbertonstudents.com>
2025-11-04 17:03:48 +0000

    ajout de files

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,6 +7,18 @@
  */
 void print_diagsums(int *a, int size)
 {
-  int i;
-  for (i = 0; i < size; i++)
-    
+int i, j;
+int sum_one = 0;
+int sum_two = 0;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+sum_one += *(a + i * size + j);
+if (i + j == size - 1)
+sum_two += *(a + i * size + j);
+}
+}
+printf("%d, %d\n", sum_one, sum_two);
+}
